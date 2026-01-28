96a2b5fe8bec484d0dea358edaaac7eb1adc4c71
devilsafe <devilsafehacker@gmail.com>
2026-01-28 09:58:28 +0100

    0.0.1

---
+++ app/src/main/java/com/lanhai/gps/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lanhai.gps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //添加自己的函数
+}
